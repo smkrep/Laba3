0b453fb7c01b28a1d601269b45400fcbd1addb5b
smkrep <m2101760@edu.misis.ru>
2023-03-26 00:30:19 +0300

    implemented pop function
    also fixed other functions and added some testcode

--- ptrList/ptrList.cpp
+++ ptrList/ptrList.cpp
@@ -21,6 +21,10 @@ void List::push(const int& val) {
 
 void List::print() const {
 	Node* ptr = head;
+	if (isEmpty()) {
+		std::cout << "The list is empty!\n";
+		return;
+	}
 	while (ptr != nullptr)
 	{
 		std::cout << ptr->data << " ";
@@ -29,4 +33,30 @@ void List::print() const {
 	std::cout << "\n";
 }
 
+void List::pop(const int& val) {
+	if (isEmpty()) {
+		std::cout << "Error! The list is already empty!\n";
+		return;
+	}
+	else {
+		Node* ptr = head;
+		while (ptr != nullptr) {
+			if (ptr->data == val) {
+				Node* onDelete = new Node();
+				onDelete = ptr;
 
+				if(ptr->next != nullptr)ptr->next->prev = ptr->prev;
+				if (ptr->prev != nullptr) ptr->prev->next = ptr->next;
+				else head = ptr->next;
+				
+				delete onDelete;
+				std::cout << "Item popped successfully\n";
+				return;
+			}
+			else {
+				ptr = ptr->next;
+			}
+		}
+		std::cout << "Error! No such item in the list\n";
+	}
+}
--- ptrList/ptrList_test.cpp
+++ ptrList/ptrList_test.cpp
@@ -10,4 +10,14 @@ int main() {
 	list.print();
 	list.push(3);
 	list.print();
+	list.pop(4);
+	list.print();
+	list.pop(4);
+	list.print();
+	list.pop(2);
+	list.print();
+	list.pop(3);
+	list.print();
+	list.push(348);
+	list.print();
 }
